2ea12a12ca0274660e3d06fdca03c207e6a05c29
febrikaerifian <57781959+febrikaerifian@users.noreply.github.com>
2026-03-12 22:04:26 +0700

    Update Layout.js

--- pos-frontend/src/components/Layout.js
+++ pos-frontend/src/components/Layout.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
-import "./layout.css";
+import "../styles/layout.css";
 
 const Layout = ({ children }) => {
 
