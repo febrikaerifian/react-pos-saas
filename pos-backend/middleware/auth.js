aa2f8afdd86c0525337cea732ccefca64ef8c242
febrikaerifian <57781959+febrikaerifian@users.noreply.github.com>
2026-03-12 18:35:17 +0700

    Update auth.js

--- pos-backend/middleware/auth.js
+++ pos-backend/middleware/auth.js
@@ -1,17 +1,29 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
 
-  if (!token) {
-    return res.status(401).json({ message: 'No token' });
+  const authHeader = req.header('Authorization');
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'No token provided' });
   }
+
+  const token = authHeader.replace('Bearer ', '');
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // ⬅️ ini penting
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET || "supersecretkey"
+    );
+
+    req.user = decoded;
+
     next();
+
   } catch (err) {
+    console.error("JWT ERROR:", err.message);
     res.status(401).json({ message: 'Invalid token' });
   }
+
 };
